4fb036a5df890944c874aa3d038ebcda6f526ef1
Tomas Monaco <tomas.monaco@wundermanthompson.com>
2021-04-14 11:21:23 -0300

    Graphql + Apollo implementation

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import CartActionTypes from './cart.types';
-import { addItemToCart, removeItemFromCart } from './cart.utils';
+import { addItemToCart, removeItemFromCart } from '../../graphql/cart.utils';
 
 const INITIAL_STATE = {
   hidden: true,
